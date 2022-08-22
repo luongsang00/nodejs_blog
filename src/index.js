76daa078344767075850435fef8c09f428f4000e
Sang Lương Tiến <18520415@gm.uit.edu.vn>
2022-08-22 21:25:06 +0700

    template engine

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const app = express();
+const port = 3000;
+//HTTP Logger
+app.use(morgan("combined"));
+//template engine
+app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+//console.log("Path: ", path.join(__dirname, "resources/views"));
+//express
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
